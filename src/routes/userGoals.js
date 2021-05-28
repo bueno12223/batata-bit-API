e95a3f82cb94996909d16c896fbae78012e48f58
Jesus Berrio <berriojesus122@gmail.com>
2021-05-28 12:23:01 -0400

    deposit money fix

--- src/routes/userGoals.js
+++ src/routes/userGoals.js
@@ -1,56 +1,18 @@
 const express = require('express');
 const userModel = require('../utils/models/user');
-const validateTransaccion = require('../utils/middleware/validateTransacction');
-const validateAuth = require('../utils/auth/validateAuth');
+//const validateTransaccion = require('../utils/middleware/validateTransacction');
+//const validateAuth = require('../utils/auth/validateAuth');
 const moment = require('moment');
 
 const userGoals = (app) => {
     const router = express.Router()
     app.use('/goal', router);
-    // get goal by id
-    router.get('/:id', async (req, res, next) => {
-        const id = req.params.id
-        try{
-            const userData = await userModel.findOne({'userPersonalData.goals._id': id});
-            const {userPersonalData: {goals}} = userData
-            const transacction = goals.filter(e => e._id == id)
-            
-            res.status(200).json({'transaccions': transacction[0] });
-        }catch(e){
-            next(e);
-            res.status(404).json({'message': 'transaccion not found'});
-        }
-    })
-    // create a goal
-    router.put('/:id', validateAuth, async(req, res, next) => {
-        const id = req.params.id;
-        const {  end, title, icon, goal } = req.body;
-        try{
-            const params = {
-                $addToSet: {
-                    "userPersonalData.goals": { 
-                        ammount : 0, 
-                        end: moment(end).format("L"), 
-                        title, 
-                        icon,
-                        goal
-                      }
-                }
-            }
-            await userModel.findByIdAndUpdate(id, params);
-            res.status(201).json({'message': 'goal created succsesfully'});
-        }catch(e){
-            next(e);
-            res.status(401).json({'message': 'all params are required or user not found'})
-        }
-        
-    })
     // put money in a goal
-    router.put('/deposit/:id',
-    (req, res, next) => validateTransaccion(req, res, next),
+    router.put('/deposit',
      async(req, res, next) => {
-        const { ammount, since } = req.body;
+        const { ammount, since, title, icon } = req.body;
         try{
+            console.log(req.body);
             const params = {
                 $inc: { 
                     'userPersonalData.goals.$.ammount' : ammount,
@@ -58,9 +20,10 @@ const userGoals = (app) => {
                     'userPersonalData.money.total' : ammount * -1
                 },
                 $addToSet: {
-                    'userPersonalData.transacctions': { to: 'you', since, ammount, type: 'goal transacction', icon }
+                    'userPersonalData.transacctions': { to: title, since: 'you', ammount, type: 'goal transacction', icon }
                 }
             }
+            
             await userModel.findOneAndUpdate({'userPersonalData.goals._id': since}, params);
             res.status(201).json({'message': 'updated done', 'ammout': ammount});
         }catch(e){
@@ -69,6 +32,30 @@ const userGoals = (app) => {
         }
         
     })
+        // create a goal
+        router.put('/:id',async(req, res, next) => {
+            const id = req.params.id;
+            const {  end, title, icon, goal } = req.body;
+            try{
+                const params = {
+                    $addToSet: {
+                        "userPersonalData.goals": { 
+                            ammount : 0, 
+                            end: moment(end).format("L"), 
+                            title, 
+                            icon,
+                            goal
+                          }
+                    }
+                }
+                await userModel.findByIdAndUpdate(id, params);
+                res.status(201).json({'message': 'goal created succsesfully'});
+            }catch(e){
+                next(e);
+                res.status(401).json({'message': 'all params are required or user not found'})
+            }
+            
+        })
     // breake goal
     router.delete('/:id', async (req, res, next) => {
 
